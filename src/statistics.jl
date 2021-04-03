fe2b8e825b3b2794e93803d2137b309392fb4a49
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2021-04-04 02:28:36 +0300

    Add phase space mean

--- src/statistics.jl
+++ src/statistics.jl
@@ -7,3 +7,27 @@ function mean_quantity(f, sim)
     end
     exec(mapvalues(mean, quantity))
 end
+
+function phase_space_mean(sim, dir::Symbol; species="electron", cond=x->true)
+    rp_mean = ThreadsX.map(sim.files) do file
+        println("Loading $(file.name)")
+        p = file["p$dir/"*species]
+        r = getdomain(p)[dir_to_idx(dir)]
+        zp = zero(eltype(p))
+        zr = zero(eltype(r))
+
+        p_mean = (p ./ length(p)) |>
+            Filter(cond) |>
+            foldxt(+, simd=true, init=zp)
+
+        r_mean = (r ./ length(r)) |>
+            Filter(cond) |>
+            foldxt(+, simd=true, init=zr)
+
+        return r_mean, p_mean
+    end
+    @time r = [rp[1] for rp in rp_mean]
+    @time p = [rp[2] for rp in rp_mean]
+
+    return r, p
+end
